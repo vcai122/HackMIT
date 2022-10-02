738a6892fb29d5f2ff3d494b95dcd4834e93db3d
vcai122 <vincent.delaware@gmail.com>
2022-10-01 23:28:16 -0400

    fix markets endpnt

--- backend/market/views.py
+++ backend/market/views.py
@@ -7,6 +7,7 @@ from django.core import serializers
 
 # Create your views here.
 from market.models import Market, Merchant, Stand, Item
+from geopy.geocoders import Nominatim
 
 
 def get_markets(request):
@@ -15,9 +16,20 @@ def get_markets(request):
         obj = {
             name: getattr(market, name) for name in ['name', 'location', 'hours', 'description']
         }
+        try:
+            locator = Nominatim(user_agent="myGeocoder")
+            target = locator.geocode(market.location)
+            location = {
+                'lat': target.latitude,
+                'lng': target.longitude
+            }
+            obj['location'] = location
+        except:
+            pass
         stand_pictures = []
         for stand in market.stand_set.all():
             stand_pictures.append(stand.picture)
         obj['stand_pictures'] = stand_pictures
         markets.append(obj)
     return JsonResponse(markets, safe=False)
+
